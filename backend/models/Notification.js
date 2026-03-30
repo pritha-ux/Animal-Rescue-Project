7fd78100575fc3189aedac81bac99e7767d7bb7a
Pritha Thapa <prithathapa09@gmail.com>
2026-03-30 22:29:33 +0545

    added status_update enum to notification model

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -4,7 +4,7 @@ const notificationSchema = new mongoose.Schema({
   recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   caseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Case' },
   message: { type: String, required: true },
-  type: { type: String, enum: ['assignment', 'update', 'alert'], default: 'update' },
+  type: { type: String, enum: ['assignment', 'update', 'alert','medical_update','status_update'], default: 'update' },
   isRead: { type: Boolean, default: false },
 }, { timestamps: true });
 
